2e244effb099dc2ce6c7652126c0666c9968932e
Brendan Armstrong <brendanarmstrong@proton.me>
2024-09-03 18:06:32 -0400

    reassign pins to keys for new layout

--- keyboards/ferris/sweep/config.h
+++ keyboards/ferris/sweep/config.h
@@ -27,12 +27,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DIRECT_PINS                                                                                          \
     {                                                                                                        \
-        {E6, F7, F6, F5, F4}, {B1, B3, B2, B6, D3}, {D1, D0, D4, C6, D7}, { B4, B5, NO_PIN, NO_PIN, NO_PIN } \
+        {D1, F6, F5, D0, D4}, {B3, B1, F7, D7, C6}, {B2, B6, E6, B4, B5}, { D5, C7, F1, NO_PIN, NO_PIN } \
     }
 
 #define DIRECT_PINS_RIGHT                                                                                    \
     {                                                                                                        \
-        {F4, F5, F6, F7, E6}, {D3, B6, B2, B3, B1}, {D7, C6, D4, D0, D1}, { B5, B4, NO_PIN, NO_PIN, NO_PIN } \
+        {D4, D0, D1, F6, F5}, {D7, B3, C6, B1, F7}, {E6, B4, B5, B6, B2}, { D5, C7, F1, NO_PIN, NO_PIN } \
     }
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
